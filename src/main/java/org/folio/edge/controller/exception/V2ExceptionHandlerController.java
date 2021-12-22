d74bd2533992f3ec440485d5a81056d678e216cc
Oleksandr_Oliinyk2 <oleksandr_oliinyk2@epam.com>
2021-12-22 11:28:26 +0200

    EDGE: Implement a new error response type. Fixed remark

--- src/main/java/org/folio/edge/controller/exception/V2ExceptionHandlerController.java
+++ src/main/java/org/folio/edge/controller/exception/V2ExceptionHandlerController.java
@@ -1,17 +1,14 @@
 package org.folio.edge.controller.exception;
 
-import java.util.stream.Collectors;
+import java.util.Optional;
 
-import org.folio.edge.domain.exception.CirculationProcessException;
-import org.folio.edge.domain.exception.EntityNotFoundException;
 import org.folio.edge.dto.InnReachResponseDTO;
-import org.folio.edge.mapper.InnReachErrorMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -20,53 +17,12 @@ import lombok.extern.log4j.Log4j2;
 @RestControllerAdvice(basePackages = "org.folio.edge.controller")
 public class V2ExceptionHandlerController {
 
-  private final InnReachErrorMapper mapper;
-
-  @ExceptionHandler(Exception.class)
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public InnReachResponseDTO handleException(Exception e) {
-    log.error("Unexpected exception: " + e.getMessage(), e);
-
-    return failed(e);
+  @ExceptionHandler(FeignException.class)
+  public ResponseEntity<InnReachResponseDTO> handleFeignStatusException(FeignException feignException) {
+    HttpStatus status = Optional.ofNullable(HttpStatus.resolve(feignException.status()))
+      .orElse(HttpStatus.INTERNAL_SERVER_ERROR);
+    InnReachResponseDTO innReachResponseDTO = new InnReachResponseDTO().status(status.toString()).reason(feignException.getMessage());
+    return new ResponseEntity<InnReachResponseDTO>(innReachResponseDTO, status);
   }
 
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public InnReachResponseDTO handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-    log.warn("Argument validation failed", e);
-
-    var bindingResult = e.getBindingResult();
-    var innReachErrors = bindingResult.getFieldErrors().stream()
-      .map(mapper::toInnReachError)
-      .collect(Collectors.toList());
-
-    return failed("Argument validation failed")
-      .errors(innReachErrors);
-  }
-
-  @ExceptionHandler(CirculationProcessException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public InnReachResponseDTO handleException(CirculationProcessException e) {
-    log.warn("Unsupported circulation operation", e);
-
-    return failed("Unsupported circulation operation");
-  }
-
-  @ExceptionHandler({ EntityNotFoundException.class, IllegalArgumentException.class})
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public InnReachResponseDTO handleEntityNotFoundException(Exception e) {
-    log.warn(e.getMessage(), e);
-
-    return failed(e);
-  }
-
-  private InnReachResponseDTO failed(Exception e) {
-    return failed(e.getMessage());
-  }
-
-  private InnReachResponseDTO failed(String reason) {
-    return new InnReachResponseDTO()
-      .status("failed")
-      .reason(reason);
-  }
 }
