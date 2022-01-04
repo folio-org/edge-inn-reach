7bd81cc03a0de42a29d24389c4e6e890661e8ef8
Oleksandr Oliinyk <oleksandr_oliinyk1@epam.com>
2022-01-04 12:59:31 +0200

    Remove accept-encoding header value for proxied calls

--- src/main/java/org/folio/edge/client/InnReachRequestInterceptor.java
+++ src/main/java/org/folio/edge/client/InnReachRequestInterceptor.java
@@ -1,5 +1,6 @@
 package org.folio.edge.client;
 
+import static org.springframework.http.HttpHeaders.ACCEPT_ENCODING;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import static org.folio.edge.external.InnReachHttpHeaders.X_D2IR_AUTHORIZATION;
@@ -11,6 +12,11 @@ public class InnReachRequestInterceptor implements RequestInterceptor {
 
   @Override
   public void apply(RequestTemplate template) {
+    updateAuthorizationHeaderName(template);
+    template.removeHeader(ACCEPT_ENCODING); // spring-cloud-feign prior to v3.1.0 doesn't handle content compression
+  }
+
+  private void updateAuthorizationHeaderName(RequestTemplate template) {
     var authorizationValue = template.headers().get(AUTHORIZATION);
     template.removeHeader(AUTHORIZATION);
     template.header(X_D2IR_AUTHORIZATION, authorizationValue);
