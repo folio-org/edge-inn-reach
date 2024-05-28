4d7b1f49d441175e7a996216a994c3dd037e987a
Gurleen Kaur <gurleen_kaur1@epam.com>
2024-05-28 12:31:46 +0530

    EDGEDCB-23 - Revert code changes of edgedcb-20

--- src/test/java/org/folio/ed/service/DcbTransactionServiceTest.java
+++ src/test/java/org/folio/ed/service/DcbTransactionServiceTest.java
@@ -11,8 +11,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.OffsetDateTime;
-
 import static org.folio.ed.utils.EntityUtils.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -47,13 +45,5 @@ class DcbTransactionServiceTest {
     Mockito.when(dcbClient.updateTransactionStatus(anyString(), any(TransactionStatus.class))).thenReturn(createTransactionStatusResponse(TransactionStatusResponse.StatusEnum.CREATED));
     dcbTransactionService.updateDCBTransactionStatus("123", createTransactionStatus(TransactionStatus.StatusEnum.OPEN));
     Mockito.verify(dcbClient).updateTransactionStatus(anyString(), any(TransactionStatus.class));
-  }
-
-  @Test
-  void getTransactionStatusListTest() {
-    var startDate = OffsetDateTime.now().minusDays(1);
-    var endDate = OffsetDateTime.now();
-    dcbTransactionService.getTransactionStatusList(startDate, endDate, 0, 100);
-    Mockito.verify(dcbClient).getTransactionStatusList(startDate.toString(), endDate.toString(), 0, 100);
   }
 }
