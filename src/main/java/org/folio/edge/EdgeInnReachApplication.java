0a14170943a77d42d8fe86c6e8cf8f1b07dd6e0a
Gurleen Kaur <34331959+gurleenkaurbp@users.noreply.github.com>
2024-05-29 18:11:35 +0530

    To release 3.2.3 (#63)
    * EDGINREACH-67: fixed BC dependency clash
    
    * [EDGINREACH-67] - Update NEWS.md and taking in changes from EDGINREACH-67
    
    * [maven-release-plugin] prepare release v3.2.3
    
    * [maven-release-plugin] prepare for next development iteration
    
    ---------
    
    Co-authored-by: Taras_Spashchenko <Taras_Spashchenko@epam.com>

--- src/main/java/org/folio/edge/EdgeInnReachApplication.java
+++ src/main/java/org/folio/edge/EdgeInnReachApplication.java
@@ -1,24 +1,21 @@
 package org.folio.edge;
 
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.security.Security;
-
-import static org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider.PROVIDER_NAME;
+import static org.folio.common.utils.tls.FipsChecker.getFipsChecksResultString;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
+@Log4j2
 public class EdgeInnReachApplication {
 
 	public static void main(String[] args) {
-    if (Security.getProvider(PROVIDER_NAME) == null) {
-      Security.addProvider(new BouncyCastleFipsProvider());
-    }
+    log.info(getFipsChecksResultString());
 		SpringApplication.run(EdgeInnReachApplication.class, args);
 	}
 }
